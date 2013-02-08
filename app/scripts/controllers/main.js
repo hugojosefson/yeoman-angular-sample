e98d4144e8cb85f79f8eb619803a8c0c3cd389fb
Hugo Josefson <hugo.josefson@jayway.com>
2013-02-08 14:31:57 +0100

    Fix manifest.appcache build error.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-yeomanAngularSampleApp.controller('MainCtrl', function($scope) {
+yeomanAngularSampleApp.controller('MainCtrl', ['$scope', function($scope) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
     'Testacular'
   ];
-});
+}]);
