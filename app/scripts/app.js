5c2ca6889a760c500aedd6f3420ba479c6f18e92
Hugo Josefson <hugo.josefson@jayway.com>
2013-03-25 20:49:24 +0100

    Use HTML5 History API.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,10 +3,11 @@
 
   var app = angular.module('yeomanAngularSampleApp', []);
 
-  app.config(function ($routeProvider) {
+  app.config(function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: '/views/main.html',
         controller: 'MainCtrl'
       })
       .otherwise({
